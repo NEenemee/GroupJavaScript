0e44c6571badd075e89f4e40c0f2c41fadc71e3a
NEenemee <113130041+NEenemee@users.noreply.github.com>
2022-12-15 11:30:30 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,27 +1,66 @@
-let express = require("express");
+
+let express = require('express');
+const { querySql } = require('./mysql/queries');
 let app = express();
-const { response } = require("express");
-const { querySql } = require("./mysql/queries");
+const mysql = require("./mysql/config.js");
+app.use(express.static("public"));
+
 app.listen(3000);
 app.use(express.static(__dirname));
 app.set("view engine", "ejs");
 
+app.get("/countries", (request, response) => {
+  let allData = ("SELECT * FROM restaurants WHERE country = ? LIMIT ?,?");
+  let take = parseInt(request.query.take);
+  let skip = parseInt(request.query.page) * take;
+  let country = request.query.country;
+  allData=  mysql.functions.format(allData, [country,skip, take]);
+  allData = querySql(allData).then(result =>{
+  response.json(result)
+  })
+});
+app.get("/cities", (request, response) => {
+  let allData = ("SELECT * FROM restaurants WHERE city = ? LIMIT ?,?");
+  let take = parseInt(request.query.take);
+  let skip = parseInt(request.query.page) * take;
+  allData=  mysql.functions.format(allData, [request.query.city]);
+  allData = querySql(allData).then(result =>{
+  response.json(result)
+  })
+});
+app.get("/cuisine", (request, response) => {
+  let allData = ("SELECT * FROM restaurants WHERE cuisine = ? LIMIT ?,?");
+  let take = parseInt(request.query.take);
+  let skip = parseInt(request.query.page) * take;
+  allData=  mysql.functions.format(allData, [request.query.cuisine]);
+  allData = querySql(allData).then(result =>{
+  response.json(result)
+  })
+});
+app.get("/places", (request, response) => {
+  let allData = ("SELECT * FROM restaurants ORDER BY name LIMIT ?,?");
+  let take = parseInt(request.query.take);
+  let skip = parseInt(request.query.page) * take;
+  allData=  mysql.functions.format(allData, [skip, take]);
+  allData = querySql(allData).then(result =>{
+  response.json(result)
+  })
+});
 app.get("/", (request, response) => {
-  let countryQuery = querySql(
-    `SELECT country AS options FROM restaurants GROUP BY country`
-  );
-  let cityQuery = querySql(
-    `SELECT city AS options FROM restaurants GROUP BY city`
-  );
-  let cuisineQuery = querySql(
-    `SELECT cuisine AS options FROM restaurants GROUP BY cuisine`
-  );
-  Promise.all([countryQuery, cityQuery, cuisineQuery]).then((sqlData) => {
-    response.render("index", {
-      title: "Resturant Search",
-      countries: sqlData[0],
-      cities: sqlData[1],
-      cuisine: sqlData[2],
+  let countryQuery = ("SELECT country AS options FROM restaurants GROUP BY country ORDER BY country ");
+  let cityQuery = (`SELECT city AS options FROM restaurants GROUP BY city ORDER BY city`);
+  let cuisineQuery = ("SELECT cuisine AS options FROM restaurants GROUP BY cuisine ORDER BY cuisine");
+  countryQuery = querySql(countryQuery);
+  cityQuery = querySql(cityQuery);
+  cuisineQuery = querySql(cuisineQuery);
+  Promise.all([countryQuery,cityQuery,cuisineQuery]).then(sqlData => {
+    response.render('index', { 
+    title: 'Resturant Search', 
+    countries: sqlData[0], 
+    cities: sqlData[1],
+    cuisine: sqlData[2], 
     });
   });
 });
+
+  
